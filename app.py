25b26bd2efbc8f8690d058d26d2ab155ccbd75df
yuramayer <votetopovorot2015@gmail.com>
2025-07-02 18:07:35 +0300

    feat: added cancel command for the bot

--- app.py
+++ app.py
@@ -3,12 +3,13 @@
 import asyncio
 from bot import bot, dp
 from utils.starting import on_startup
-from handlers import start_admin_router, manual_food_router, auto_food_router
+from handlers import start_admin_router, manual_food_router, \
+    auto_food_router, cancel_router
 
 
 async def main():
     """Bot startup function"""
-    dp.include_routers(start_admin_router,
+    dp.include_routers(start_admin_router, cancel_router,
                        manual_food_router, auto_food_router)
     dp.startup.register(on_startup)
     await bot.delete_webhook(drop_pending_updates=True)
--- handlers/__init__.py
+++ handlers/__init__.py
@@ -5,3 +5,4 @@
 from .start_admin import start_admin_router
 from .auto_food import auto_food_router
 from .manual_food import manual_food_router
+from .cancel import cancel_router
